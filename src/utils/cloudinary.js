c58908afa8e9e15a763cddae444d6f59bf32ae5c
Sougata Jana <sougataj76@gmail.com>
2025-07-05 19:51:06 +0530

    Implement cloudinary image upload utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_KEY_SECRET
+});
+
+const uploadCloudinary = async (filepath)=>{
+  try {
+    if(!filepath) return null;
+    // the file upload in cloudinary
+   const responce = await cloudinary.uploader.upload(filepath,{
+      resource_type:'auto'
+    })
+    
+    // file has been successfully
+    console.log("the file is uploaded on cloudinary");
+    
+  }catch (error) {
+    fs.unlinkSync(filepath);// delete the file from local storage
+    return null;
+  }
+  
+}
