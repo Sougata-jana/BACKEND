d409bcf7f1c955a1422de89412acca353fe3bb3c
Sougata Jana <sougataj76@gmail.com>
2025-07-08 20:37:11 +0530

    Refactor registerUser function to include detailed comments and improve clarity; simplify multer export syntax

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req, res) =>{
-    res.status(300).json({
-        message: "ok",
-        user: "souagat jana"
-    })
+    // get user details from request body 
+    // validation are - empty
+    // check if user already exists - emial, username
+    // check for iamge , check for avatar
+    // upload them to cloudinary ,avatar
+    // create user object, create enetry in db
+    // remove the passwird, refresh token fields form responce 
+    // check for user creation 
+    // return responce
+
+
+ const {username, fullname, email, password}  = req.body 
+        console.log("email:", email);
+    
+
+
 })
 
 export {registerUser}
